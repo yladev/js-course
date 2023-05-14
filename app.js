6a7fe460b20696819fd375429af747d4ecc49a2a
yladev <yanlinnaung881@gmail.com>
2023-05-14 15:33:50 +0630

    update naming variables

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 import { answer } from "./addingtwovar.js";
+import { c, z } from "./variables.js";
+
 console.log("Answer => " + answer);
+
+console.log("Subtraction two vars => " + c);
+
+console.log(z);
---
+++ variables.js
@@ -0,0 +1,16 @@
+var a = 5;
+var b = 4;
+export var c = a - b;
+
+let x;
+x = 6;
+x = 5;
+x = 4;
+let y = 3;
+export let z = x + y;
+
+let firstNumber = 7;
+let secondNumber = 3;
+let firstName = "Jason";
+let zipcode = "75082";
+let secondZipCode = "60459";
