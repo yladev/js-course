f88314dfecdbf163d3bb7279c1ae7fbedd2ba089
yladev <yanlinnaung881@gmail.com>
2023-05-14 17:07:07 +0630

    update type conversion

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { answer } from "./addingtwovar.js";
+import { d, e, num3 } from "./coercion.js";
 import { j, k, l } from "./datatypes.js";
 import { c, z } from "./variables.js";
 
@@ -11,3 +12,7 @@ console.log(z);
 console.log(typeof j);
 console.log(typeof k);
 console.log(typeof l);
+
+console.log(num3);
+console.log(d);
+console.log(e);
---
+++ coercion.js
@@ -0,0 +1,7 @@
+let num1 = 7;
+let num2 = "6";
+num2 = parseInt(num2, 10);
+export let num3 = num1 + num2;
+
+export let d = parseInt("bob", 10);
+export let e = isNaN(d);
