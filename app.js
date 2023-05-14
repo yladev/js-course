55397798523d06813ab568b05a65a3d730124011
yladev <yanlinnaung881@gmail.com>
2023-05-14 16:52:57 +0630

    update datatypes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { answer } from "./addingtwovar.js";
+import { j, k, l } from "./datatypes.js";
 import { c, z } from "./variables.js";
 
 console.log("Answer => " + answer);
@@ -6,3 +7,7 @@ console.log("Answer => " + answer);
 console.log("Subtraction two vars => " + c);
 
 console.log(z);
+
+console.log(typeof j);
+console.log(typeof k);
+console.log(typeof l);
---
+++ datatypes.js
@@ -0,0 +1,5 @@
+export let j = 5;
+
+export let k = "Yan Linn Aung";
+
+export let l = true; // true or false
